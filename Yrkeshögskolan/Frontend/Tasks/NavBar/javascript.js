846a91dfff4c6ceb9c9f2754ad3742522bcd8726
Zaaiiko <zakarias.rif.berggren@gmail.com>
2023-09-27 02:01:39 +0200

    learned some new stuff

---
+++ Yrkeshögskolan/Frontend/Tasks/NavBar/javascript.js
@@ -0,0 +1,10 @@
+console.log("konsollogar window objektet", window);
+
+let names = "zakarias";
+const lastName = "Berggren";
+
+console.log(names, lastName);
+
+let year = 33;
+const days = 44;
+console.log (year + days);
